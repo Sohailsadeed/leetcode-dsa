e3042893673340996f1065c19c7763fab897477b
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-06-14 22:36:13 +0530

    Time: 20 ms (40.36%), Space: 48.1 MB (7.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,10 +3,13 @@ class Solution {
         int l = 0, r = 0, maxLen = 0, start = 0;
         HashMap<Character, Integer> required = new HashMap<>();
         HashMap<Character, Integer> window = new HashMap<>();
+
         for( int i = 0; i < t.length(); i++){
             required.put(t.charAt(i), required.getOrDefault(t.charAt(i), 0) + 1);
             }
+
         int requiredCount = required.size(), formed = 0, minLength = Integer.MAX_VALUE;
+
         while(r < s.length()){
             window.put(s.charAt(r), window.getOrDefault(s.charAt(r), 0) + 1);
             char ch = s.charAt(r);
@@ -14,13 +17,14 @@ class Solution {
                  window.get(ch).equals(required.get(ch))) {
                 formed++;
             }
+
             while(formed == requiredCount){
                 if((r - l + 1) < minLength){
                 minLength = r - l + 1;
                 start = l;
                 }
                 char temp = s.charAt(l);
-                window.put(s.charAt(l), window.get(s.charAt(l)) - 1);
+                window.put(temp, window.get(temp) - 1);
                 if(required.containsKey(temp) && window.getOrDefault(temp, 0) < required.get(temp))
                     formed--;
                 l++;
