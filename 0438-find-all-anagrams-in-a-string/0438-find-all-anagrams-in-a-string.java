bfed494a772ea781d232ef10719d26a4075db7cc
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-06-15 19:53:53 +0530

    Time: 8 ms (94.92%), Space: 46.7 MB (70.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int r = 0, l = 0;
-        List<Integer> res = new ArrayList<>();
-        int[] required = new int[26];
+        int n1 = p.length(), n2 = s.length();
         int[] window = new int[26];
-        for(int i = 0 ; i < p.length() ; i ++){
-           required[p.charAt(i) - 'a']++;
+        int[] required = new int[26];
+        List<Integer> res = new ArrayList<>();
+        if(n1 > n2)
+            return res;
+        for(int i = 0;i < n1 ; i++){
+            required[p.charAt(i) - 'a']++;
+            window[s.charAt(i) - 'a']++;
         }
-        while(r < s.length()){
-            window[s.charAt(r) - 'a']++;
-            while( (r - l + 1) > p.length()){
-                char ch = s.charAt(l);
-                window[ch - 'a']--;
-                l++;
-            }
-            if(((r - l + 1) == p.length()) && Arrays.equals(window, required))
-                res.add(l);
-            r++;
+        if(matches(required, window))
+            res.add(0);
+
+        for(int i = n1 ; i < n2 ; i++){
+            window[s.charAt(i) - 'a']++;
+            window[s.charAt(i - n1) - 'a']--;
+            if(matches(required, window))
+                res.add(i - n1 + 1);
         }
         return res;
+    }
+
+    private boolean matches(int[] required, int[] window){
+        for(int i = 0 ; i < 26 ; i ++ )
+            if(required[i] != window[i])
+                return false;
+        return true;
     }
 }
