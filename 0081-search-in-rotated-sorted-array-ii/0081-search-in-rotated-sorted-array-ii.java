b845fe9441ce61e411ab18603d7f7e60f58757d4
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-07-16 07:13:57 +0530

    Time: 0 ms (100%), Space: 44.8 MB (82.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -11,17 +11,16 @@ class Solution {
                 high--;
                 continue;
             }
-           if (nums[mid] <= nums[high]) {
-                if (target > nums[mid] && target <= nums[high])
-                    low = mid + 1;
-                else
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
-            } 
-            else {
-                if (target >= nums[low] && target < nums[mid])
-                    high = mid - 1;
-                else
+                } else
                     low = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else
+                    high = mid - 1;
             }
         }
         return false;
