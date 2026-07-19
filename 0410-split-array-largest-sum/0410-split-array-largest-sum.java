39337caca118f05df2ce3d24db909b380e02efe5
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-07-19 11:41:14 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.12%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
         int low = Integer.MIN_VALUE, high = 0, mid = 0;
-        int res = Integer.MAX_VALUE;
         for(int num : nums){
             low = Integer.max(low, num);
             high += num;
@@ -19,12 +18,12 @@ class Solution {
     }
 
     private int findMaxSplits(int[] nums, int threshold){
-        int curSplit = 1, sum = 0;
-        for(int num : nums){
-            sum += num;
+        int curSplit = 1, sum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            sum += nums[i];
             if(sum > threshold){
                 curSplit++;
-                sum = num;
+                sum = nums[i];
             }
         }
         return curSplit;
