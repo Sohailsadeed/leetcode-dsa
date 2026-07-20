9b3afa95992f58352525635e98f68824a933dfde
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-07-20 19:55:10 +0530

    Time: 112 ms (8.38%), Space: 81.3 MB (17.04%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int low = 1, high = Integer.MIN_VALUE, mid = 0;
+        for(int quantity : quantities)
+            high = Integer.max(high, quantity);
+        while(low < high){
+            mid = (low + high) >> 1;
+            int maxStores = findStores(quantities, mid);
+            if(maxStores <= n)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+
+    private int findStores(int[] quantities, int threshold){
+        int storeCount = 0;
+        for(int i = 0; i < quantities.length; i++){
+            storeCount += Math.ceil((double) quantities[i] / threshold);
+        }
+        return storeCount;
+    }
+}
