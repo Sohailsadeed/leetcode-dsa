fc3df054bd9461957f6d6134a5a8f23d9cf80cd7
Mohammad Sohail Ahmed <ahmedsohail70870@gmail.com>
2026-09-25 23:38:47 +0530

    Time: 23 ms (53.48%), Space: 46.6 MB (38.78%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,61 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        return sumMax(nums) - sumMin(nums);
+    }
+
+    private long sumMin(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() &&
+                  (i == n || nums[stack.peek()] >= nums[i])) {
+
+                int mid = stack.pop();
+
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int right = i;
+
+                long leftCount = mid - left;
+                long rightCount = right - mid;
+
+                sum += (long) nums[mid] * leftCount * rightCount;
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return sum;
+    }
+
+    private long sumMax(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() &&
+                  (i == n || nums[stack.peek()] <= nums[i])) {
+
+                int mid = stack.pop();
+
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int right = i;
+
+                long leftCount = mid - left;
+                long rightCount = right - mid;
+
+                sum += (long) nums[mid] * leftCount * rightCount;
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return sum;
+    }
+}
